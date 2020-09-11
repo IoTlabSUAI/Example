e5bf349b6228bf8c93930b9e5d725a16f8e08da8
DevicesSuai <56974148+DevicesSuai@users.noreply.github.com>
2020-09-11 15:29:12 +0300

    add sqr func

--- Example/Source.cpp
+++ Example/Source.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+constexpr int sqr(int x) {
+	return x * x;
+}
 
 int main() {
-	constexpr int  y = 5 * 5;
+	constexpr int  y = sqr(5);
 	printf("Result: %u", y);
 	return 0;
 }
